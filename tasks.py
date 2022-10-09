6a0fdbfc5972f0af2337a8578ee95d90ce81992f
Viktor Eikman <viktor.eikman@gmail.com>
2022-10-09 12:07:35 +0200

    Add a canonical package build process

--- tasks.py
+++ tasks.py
@@ -10,9 +10,16 @@ from invoke import task
 
 
 @task()
+def snapshot_hissp(c):
+    """Install the latest development snapshot of Hissp."""
+    c.run('pipenv run '
+          'pip install -U git+https://github.com/gilch/hissp')
+
+
+@task()
 def compile(c):
     """Compile bundled Lissp code to Python."""
-    c.run('python -c "'
+    c.run('pipenv run python -c "'
           'from hissp import transpile; '
           'import lisscad; '
           r'transpile(lisscad.__package__, \"prelude\")'
@@ -22,7 +29,18 @@ def compile(c):
 @task(pre=[compile], default=True)
 def test(c):
     """Run unit tests."""
-    c.run('pytest', pty=True)
+    c.run('pipenv run pytest', pty=True)
+
+
+@task()
+def build(c):
+    """Build for distribution.
+
+    The build process is based on setuptools controlled via the “build”
+    package.
+
+    """
+    c.run('python -m build')
 
 
 @task
